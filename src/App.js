350b7ee7884f0ad3adc7d065ca2ec946ef619d0e
Junseok Lee <lee6716@gmail.com>
2023-04-05 19:23:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Home from "./component/home/Home";
+import About from "./component/about/About";
+import Sidebar from "./component/sidebar/Sidebar";
+import Services from "./component/services/Services";
+import Resume from "./component/resume/Resume";
+import Portfolio from "./component/portfolio/Portfolio";
+import Pricing from "./component/pricing/Pricing";
+import Testimonials from "./component/testimonials/Testimonials";
+import Blog from "./component/blog/Blog";
+import Contact from "./component/contact/Contact";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portfolio />
+        <Pricing />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
+    </>
+  );
+};
+
+export default App;
